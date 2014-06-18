01427d0f372bc241cfd4e15f52093af175734616
Fulanke <franck@MacBook-Pro-de-Franck.local>
2014-06-18 22:25:21 +0200

    Merge branch 'master' of https://github.com/franck
    Conflicts:
    app/views/directives/timeLine.html
    app/views/partials/timeline.html

--- app/scripts/controllers/actionCtrl.js
+++ app/scripts/controllers/actionCtrl.js
@@ -3,9 +3,7 @@ angular.module('compassApp')
   .controller('ActionCtrl', function ($scope, Auth) {
     $scope.droppedObjects = [];
 
-
-
-    $scope.getPicto = function(){
+    $scope.getPicto = function(index){
       var pic = [
           'glyphicon glyphicon-asterisk',
           'glyphicon glyphicon-plus',
@@ -18,104 +16,117 @@ angular.module('compassApp')
           'glyphicon glyphicon-music',
           'glyphicon glyphicon-search'
       ];
-      return pic[Math.floor((Math.random()*9))];
+      return pic[index];
     };
 
     $scope.LINKEDACTIONS = [
       {
         name: 'action 1',
-        img: $scope.getPicto(),
+        img: $scope.getPicto(1),
         date: "T3-2030",
         building: 'building 1',
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: true,
-        id: 1
+        id: 1,
+        type: 1
       },
       {
         name: 'action 2',
-        img: $scope.getPicto(),
+        img: $scope.getPicto(2),
         building: 'building 1',
         date: "T0-2030",
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: true,
-        id: 2
+        id: 2,
+        type: 2
       },
       {
         name: 'action 3',
         date: "T0-2030",
-        img: $scope.getPicto(),
+        img: $scope.getPicto(3),
         building: 'building 1',
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: true,
-        id: 3
+        id: 3,
+        type: 3
       },
       {
         name: 'action 4',
-        img: $scope.getPicto(),
+        img: $scope.getPicto(4),
         date: "T3-2030",
         building: 'building 1',
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: false,
-        id: 4
+        id: 4,
+        type: 4
       },
       {
         name: 'action 4',
-        img: $scope.getPicto(),
+        img: $scope.getPicto(4),
         date: "T3-2030",
         building: 'building 1',
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: false,
-        id: 5
+        id: 5,
+        type: 4
       },
       {
         name: 'action 5',
-        img: $scope.getPicto(),
+        img: $scope.getPicto(5),
         date: "T1-2014",
         building: 'building 1',
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: false,
-        id: 6
+        id: 6,
+        type: 5
       },
       {
         name: 'action 5',
-        img: $scope.getPicto(),
+        img: $scope.getPicto(5),
         date: "T1-2014",
         building: 'building 3',
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: false,
-        id: 7
+        id: 7,
+        type: 5
       },
       {
         name: 'action 6',
-        img: $scope.getPicto(),
+        img: $scope.getPicto(6),
         date: "T2-2015",
         building: 'building 1',
         cost: '20204.45',
         efficiency: 34,
         tri: 23,
         done: true,
-        id: 8
+        id: 8,
+        type: 6
       }
     ];
 
 
-
     $scope.stock={};
-    var test = function() {
+    
+    $scope.$watch($scope.LINKEDACTIONS, function(){      
+      $scope.genStock();
+    })
+
+    $scope.genStock = function() {
+      $scope.stock={};
       for(action in $scope.LINKEDACTIONS) {
           var tmpAction = $scope.LINKEDACTIONS[action];
 
@@ -123,13 +134,13 @@ angular.module('compassApp')
             $scope.stock[tmpAction.date] = {};
           }
 
-          if(!$scope.stock[tmpAction.date][tmpAction.name]){
-            $scope.stock[tmpAction.date][tmpAction.name] = [];
+          if(!$scope.stock[tmpAction.date][tmpAction.type]){
+            $scope.stock[tmpAction.date][tmpAction.type] = [];
           }
-            $scope.stock[tmpAction.date][tmpAction.name].push(tmpAction);
+            $scope.stock[tmpAction.date][tmpAction.type].push(tmpAction);
           }
     };
-    test();
+    //$scope.genStock();
     
    $scope.ACTIONS = [
     {
