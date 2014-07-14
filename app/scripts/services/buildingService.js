2a8b6e9013dd3d268d3756ec05292368d1544ef5
Sergii Tsegelnyk <dr3am3r.ua@gmail.com>
2014-07-14 21:32:49 +0300

    improvs

--- app/scripts/services/buildingService.js
+++ app/scripts/services/buildingService.js
@@ -8,55 +8,69 @@ angular.module('compassApp').service('buildingService', ['$http', function($http
   var self = this;
   // dummy data atm
   //this.url = '/test_data/buildings.json';
-  
-  this.getAll = function() {
-    return [
+
+  this.buildings = [
             {
+              "id": 1,
               "name": "Bâtiment 1",
               "usage": "A",
               "address": "whatever street",
-              "coords": {
-                "latitude": 45,
-                "longitude": -73
-              }
+              "latitude": 45,
+              "longitude": -73
             },
             {
+              "id": 2,
               "name": "Bâtiment 2",
               "usage": "E",
               "address": "whatever street",
-              "coords": {
-                "latitude": 45.23,
-                "longitude": -73.123
-              }
+              "latitude": 45.23,
+              "longitude": -73.123
             },
             {
+              "id": 3,
               "name": "Bâtiment 3",
               "usage": "B",
               "address": "whatever street",
-              "coords": {
-                "latitude": 45.34,
-                "longitude": -73.023
-              }
+              "latitude": 45.34,
+              "longitude": -73.023
             },
             {
+              "id": 4,
               "name": "Bâtiment 4",
               "usage": "C",
               "address": "whatever street",
-              "coords": {
-                "latitude": 45.13,
-                "longitude": -73.123
-              }
+              "latitude": 45.13,
+              "longitude": -73.123
             },
             {
+              "id": 5,
               "name": "Bâtiment 5",
               "usage": "D",
               "address": "whatever street",
-              "coords": {
-                "latitude": 45.03,
-                "longitude": -73.103
-              }
+              "latitude": 45.03,
+              "longitude": -73.103
             }
           ];
+
+  this.getAll = function() {
+    return this.buildings;
   };
+
+  this.getUsageTypes = function() {
+    var usageTypes = {},
+      ret = [];
+
+    for (var i=0, l=this.buildings.length; i<l; i++) {
+      usageTypes[this.buildings[i].usage] = true;
+    }
+
+    for (var prop in usageTypes) {
+      if (usageTypes.hasOwnProperty(prop)) {
+        ret.push(prop);
+      }
+    }
+
+    return ret;
+  }
 
 }]);
