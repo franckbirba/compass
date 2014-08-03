0e8e55687121226e64e068aee7c1de499766fbcf
Sergii Tsegelnyk <dr3am3r.ua@gmail.com>
2014-08-03 19:04:26 +0300

    rm collapicker from repo

--- app/scripts/directives/collapicker.js
+++
@@ -1,7 +0,0 @@
-angular.module('compassApp').directive('colorPicker', function () {
-  return {
-        restrict: 'E',
-        //templateUrl:'/views/directives/collapicker.html'
-        template: '<h1>Colla picker!</h1>'
-    }
-});
